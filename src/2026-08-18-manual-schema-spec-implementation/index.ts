b7517347623d9e5ccbb539d6f70b50d7f75d6b52
Stephan Kulla <git.mail@kulla.me>
2026-09-09 18:43:23 +0200

    wip: manual spec impl

--- src/2026-08-18-manual-schema-spec-implementation/index.ts
+++ src/2026-08-18-manual-schema-spec-implementation/index.ts
@@ -1,36 +1,55 @@
-// schema.ts
+// Value types
 
-interface Schema<Value> {
-  readonly kind: string
-  readonly __value?: Value
+interface Value {
+  readonly type: string;
 }
 
-type AnySchema = Schema<unknown>
-type ValueOf<S extends AnySchema> = S extends Schema<infer Value> ? Value : never
+interface StringValue extends Value {
+  readonly type: "string";
+  get(): string;
+  set(value: string): void;
+}
+
+interface ArrayValue<C extends Value> {
+  readonly type: "array";
+  map<R>(mapper: (child: C) => R): R;
+}
+
+// schema.ts
+
+interface Schema<V extends Value = Value> {
+  readonly kind: string;
+  readonly __value?: V;
+}
+
+type ValueOf<S extends Schema> = S extends Schema<infer Value> ? Value : never;
 
 // schema/string
 
-interface StringSchema extends Schema<string> {
-  readonly kind: "string"
+interface StringSchema extends Schema<StringValue> {
+  readonly kind: "string";
 }
 
-const string = (): StringSchema => ({ kind: "string" })
+const string = (): StringSchema => ({ kind: "string" });
 
 // schema/array
 
-interface ArraySchema<C extends AnySchema> extends Schema<ValueOf<C>[]> {
-  readonly kind: "array"
-  readonly min?: number
-  readonly max?: number
-  readonly element: C
+interface ArraySchema<C extends Schema> extends Schema<ArrayValue<ValueOf<C>>> {
+  readonly kind: "array";
+  readonly min?: number;
+  readonly max?: number;
+  readonly element: C;
 }
 
-const array = <C extends AnySchema>(element: C, options?: { max?: number; min?: number }) => ({
+const array = <C extends Schema>(
+  element: C,
+  options?: { max?: number; min?: number },
+) => ({
   kind: "array",
   element,
   ...options,
-})
+});
 
 // example usage
 
-export const tags = array(string(), { min: 1, max: 5 })
+export const tags = array(string(), { min: 1, max: 5 });
