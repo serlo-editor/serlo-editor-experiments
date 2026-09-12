5d0a77a7e62639fc04ebd5b410c4a797f3d98c42
Stephan Kulla <git.mail@kulla.me>
2026-09-12 11:28:38 +0200

    feat(schema): add JSON value typing and storage interfaces

--- src/2026-08-18-manual-schema-spec-implementation/index.ts
+++ src/2026-08-18-manual-schema-spec-implementation/index.ts
@@ -17,20 +17,27 @@ interface ArrayValue<C extends Value> {
 
 // schema.ts
 
-interface Schema<V extends Value = Value> {
+interface Schema<V extends Value = Value, JSONValue = unknown> {
   readonly kind: string;
   readonly __value?: V;
+  readonly __jsonValue?: JSONValue;
 }
 
-type ValueOf<S extends Schema> = S extends Schema<infer Value> ? Value : never;
+type ValueOf<S extends Schema> =
+  S extends Schema<infer Value, unknown> ? Value : never;
+type JSONValueOf<S extends Schema> =
+  S extends Schema<Value, infer JSONValue> ? JSONValue : never;
 
-interface StringSchema extends Schema<StringValue> {
+interface StringSchema extends Schema<StringValue, string> {
   readonly kind: "string";
 }
 
 const string = (): StringSchema => ({ kind: "string" });
 
-interface ArraySchema<C extends Schema> extends Schema<ArrayValue<ValueOf<C>>> {
+interface ArraySchema<C extends Schema> extends Schema<
+  ArrayValue<ValueOf<C>>,
+  JSONValueOf<C>[]
+> {
   readonly kind: "array";
   readonly min?: number;
   readonly max?: number;
@@ -45,6 +52,22 @@ const array = <C extends Schema>(
   element,
   ...options,
 });
+
+// Storage
+
+type Branded<T, S> = T & { __branded: S };
+type Reference = Branded<string, "Reference">;
+
+interface StorageAdapter<R extends Reference> {
+  get(reference: R): string;
+  set(reference: R, value: string): void;
+  getChildren(reference: R): R[];
+}
+
+interface Storage {
+  save<S extends Schema>(schema: S, value: JSONValueOf<S>): Reference;
+  load<S extends Schema>(schema: S, reference: Reference): JSONValueOf<S>;
+}
 
 // example usage
 
