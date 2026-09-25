b9e24e7da2df07c6eea3f6ced083987931e2d38d
Stephan Kulla <git.mail@kulla.me>
2026-09-25 16:21:23 +0200

    feat(json-storage): add typed JSON storage save interface

--- src/2026-09-21-json-storage-implementation/json-storage.ts
+++ src/2026-09-21-json-storage-implementation/json-storage.ts
@@ -2,7 +2,16 @@
 
 type JSONValue = boolean | string | JSONValue[] | { [key: string]: JSONValue }
 type ValueType = "boolean" | "string" | "array" | "object"
-type ValueOf<V extends BaseValue> = V extends BaseValue<infer Serialized> ? Serialized : never
+type JSONValueOf<V extends BaseValue> = V extends BaseValue<infer Serialized> ? Serialized : never
+type ValueOf<Serialized extends JSONValue> = Serialized extends boolean
+  ? BooleanValue
+  : Serialized extends string
+    ? StringValue
+    : Serialized extends JSONValue[]
+      ? ArrayValue<ValueOf<Serialized[number]>>
+      : Serialized extends { [key: string]: JSONValue }
+        ? ObjectValue<{ [Key in keyof Serialized]: ValueOf<Serialized[Key]> }>
+        : never
 
 interface BaseValue<Serialized extends JSONValue = JSONValue> {
   readonly type: ValueType
@@ -19,15 +28,21 @@ interface StringValue extends BaseValue<string> {
   set(value: string): void
 }
 
-interface ArrayValue<T extends Value = Value> extends BaseValue<ValueOf<T>[]> {
+interface ArrayValue<T extends Value = Value> extends BaseValue<JSONValueOf<T>[]> {
   readonly type: "array"
   map<R>(fn: (value: T, index: number) => R): R[]
 }
 
 type ObjectValue<Properties extends { [key: string]: Value } = {}> = BaseValue<{
-  [Key in keyof Properties]: ValueOf<Properties[Key]>
+  [Key in keyof Properties]: JSONValueOf<Properties[Key]>
 }> & {
   readonly type: "object"
 } & Properties
 
 type Value = BooleanValue | StringValue | ArrayValue | ObjectValue
+
+// Storage
+
+interface JSONStorage {
+  save<Serialized extends JSONValue>(value: Serialized): ValueOf<Serialized>
+}
